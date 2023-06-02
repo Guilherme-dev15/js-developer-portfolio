396f31d6c040c1b6b3236e08e66ec129748d926c
Guilherme_Dev15 <guilherme.macedo1598@gmail.com>
2023-06-02 19:12:23 -0300

    update json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData (){
-    const url = 'https://raw.githubusercontent.com/Guilherme-dev15/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/Guilherme-dev15/js-developer-portfolio/master/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
