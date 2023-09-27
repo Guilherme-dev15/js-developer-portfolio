e650cdcea36d48b7252255e8034b583c510c5b71
Guilherme Anjos <49658386+Guilherme-dev15@users.noreply.github.com>
2023-09-27 15:55:53 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,9 @@ function updateProfileInfo(profileData) {
     const location = document.getElementById('profile-location')
     location.innerText = profileData.location
 
-
+    const phone = document.getElementById('profile-phone')
+    phone.innerText = profileData.phone
+    phone.href = `tel:${profileData.phone}`
 
     const email = document.getElementById('profile-email')
     email.innerText = profileData.email
