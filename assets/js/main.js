541cd6e830287dfa0aee6355e209afc6f5c80539
Guilherme Anjos <49658386+Guilherme-dev15@users.noreply.github.com>
2024-09-30 09:59:13 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,14 @@ function updateSoftSkills(profileData) {
 
 function updateHardSkills(profileData) {
     const hardSkills = document.getElementById('profile.skills.hardSkills')
-    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => 
+    `<li>
+        <div>
+            <img src="${skill.logo}" alt="${skill.name}" title="${skill.name}" style="width: 50px; height: 50px;" />
+            ${skill.name}
+        </div>
+    </li>`
+).join('');
 }
 
 function updateLanguages(profileData) {
