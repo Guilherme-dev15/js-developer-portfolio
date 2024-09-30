c85dd3e338159a6074e266cf914d60f9b846bcb4
Guilherme Anjos <49658386+Guilherme-dev15@users.noreply.github.com>
2024-09-30 10:52:57 -0300

    refator

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,12 +27,8 @@ function updateSoftSkills(profileData) {
 }
 
 function updateHardSkills(profileData) {
-    const hardSkills = document.getElementById('profile-skills-hardSkills'); 
- 
-    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `
-        <li>
-            <img src="${skill.logo}" alt="${skill.name}" title="${skill.name}">
-            ${skill.name}  </li>)`.join('')
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 
 function updateLanguages(profileData) {
